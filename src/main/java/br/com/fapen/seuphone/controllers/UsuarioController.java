7bf2f094542a9247ea3148d2d2404bd15f532670
Emerson Melo Martins <emerson25xd@gmail.com>
2020-05-08 13:08:32 -0300

    Atualização imagem perfil ao trocar

--- src/main/java/br/com/fapen/seuphone/controllers/UsuarioController.java
+++ src/main/java/br/com/fapen/seuphone/controllers/UsuarioController.java
@@ -6,6 +6,9 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -148,6 +151,9 @@ public class UsuarioController {
 		Usuario usuario = usuarioRep.findByLogin(principal.getName());
 		usuario.setCaminhoFoto(caminhoDaFoto);
 		usuarioRep.save(usuario);
+		
+		Authentication authentication = new UsernamePasswordAuthenticationToken(usuario, usuario.getSenha(), usuario.getAuthorities());
+		SecurityContextHolder.getContext().setAuthentication(authentication);
 		
 		return "redirect:/usuarios/meuperfil";
 	}
